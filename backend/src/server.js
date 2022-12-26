fdc3306b4d29e1048645f71568c4af81c8a61c59
beatriceev <beatriceprayogo31@gmail.com>
2022-12-26 14:41:18 +0800

    deploy10

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,13 +32,15 @@ const database = mongoose.connection;
 //       res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
 //     });
 // }
-app.use(cors());
 app.use("/api", routes);
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "../frontend", "build")));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, "../frontend", "build")));
+    app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+    });
+}
 
 const port = process.env.PORT || 4000
 
