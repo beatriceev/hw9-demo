a07a82232ebe2426cd89eee1beb0c927098db52d
beatriceev <beatriceprayogo31@gmail.com>
2022-12-26 14:32:47 +0800

    deploy9

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,9 +35,9 @@ const database = mongoose.connection;
 app.use(cors());
 app.use("/api", routes);
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../frontend", "build")));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
 });
 
 const port = process.env.PORT || 4000
